d31462d78151bd0bedbcc7aea405d5ce5f37a8b9
Benyamin Limanto <benyamin.limanto@salt.co.id>
2023-08-23 11:08:36 +0700

    test(user-detail-page): introduced

--- src/app/users/users-detail-page/users-detail-page.component.spec.ts
+++ src/app/users/users-detail-page/users-detail-page.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersDetailPageComponent } from './users-detail-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersDetailComponent } from '../users-detail/users-detail.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from 'src/services/user-service';
 
 describe('UsersDetailPageComponent', () => {
   let component: UsersDetailPageComponent;
@@ -8,14 +12,34 @@ describe('UsersDetailPageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UsersDetailPageComponent]
+      declarations: [UsersDetailPageComponent, UsersDetailComponent],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [UserService]
     });
     fixture = TestBed.createComponent(UsersDetailPageComponent);
     component = fixture.componentInstance;
+    component.id = 5;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain data after init', (done: DoneFn) => {
+    component.ngOnInit();
+    setTimeout(() => {
+      expect(component.d).not.toBeUndefined();
+      done();
+    }, 1500);
+  });
+
+  it('should be null when wrong id sent to component', (done: DoneFn) => {
+    component.id = undefined;
+    component.ngOnInit();
+    setTimeout(() => {
+      expect(component.d).not.toBeUndefined();
+      done();
+    }, 1500);
   });
 });
