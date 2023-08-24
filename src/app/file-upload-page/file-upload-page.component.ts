ddf40d5f11f46c5604bfacca35818aa182277c2d
Benyamin Limanto <benyamin.limanto@salt.co.id>
2023-08-24 12:03:05 +0700

    feat(file-upload): FormArray and FormGroup

--- src/app/file-upload-page/file-upload-page.component.ts
+++ src/app/file-upload-page/file-upload-page.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { fileRequiredValidator } from 'src/shared/form-validators/file-required.validators';
 
-interface FileUploadFormInterface
-{
-  files: FormControl
-}
+// interface FileUploadFormInterface
+// {
+//   files: FormControl
+// }
 
 @Component({
   selector: 'app-file-upload-page',
@@ -13,29 +13,50 @@ interface FileUploadFormInterface
   styleUrls: ['./file-upload-page.component.scss']
 })
 export class FileUploadPageComponent {
-  public fileUploadForm: FormGroup<FileUploadFormInterface> = new FormGroup<FileUploadFormInterface>({
-    files: new FormControl('', fileRequiredValidator())
+  public fileUploadForm: FormGroup = new FormGroup({
+    files: new FormArray([]),
+    individualFile: new FormControl('', [fileRequiredValidator()])
   });
 
   ngOnInit() {
-    this.fileUploadForm.valueChanges.subscribe((v) => {
-      console.log(v);
-    });
+    // this.fileUploadForm.valueChanges.subscribe((v) => {
+    //   console.log(v);
+    // });
+  }
+
+  public AddFileUploadField()
+  {
+    let files = this.fileUploadForm.controls['files'] as FormArray;
+    files.push(new FormGroup({file: new FormControl('', [fileRequiredValidator()])}));
+    console.log(files);
   }
 
   public Submit()
   {
-    console.log(this.fileUploadForm.value.files);
+    console.log(this.fileUploadForm);
   }
 
-  public OnFileChange(event: Event)
+  public OnFileChangeIndividual(event: Event)
+  {
+    if(event.type == "cancel") 
+    {
+      this.fileUploadForm.controls['individualFile'].markAsDirty();
+      this.fileUploadForm.setValue({individualFile: '', files: this.fileUploadForm.value.files});
+    }
+    console.log(this.fileUploadForm.value);
+  }
+
+  public OnFileChange(event: Event, ix: number)
   {
     // To Clear file select when... 
     // Cancel Select File
     if(event.type == "cancel") 
     {
-      this.files?.markAsDirty();
-      this.fileUploadForm.setValue({files: ''});
+      this.filesArray[ix].controls['file'].markAsDirty();
+      this.filesArray[ix].setValue({file: ''});
+      console.log(this.filesArray[ix].value);
+      // SUCK HACK
+      (event.target as HTMLInputElement).value = "";
     }
     // console.log(event);
     // console.log(event.target);
@@ -45,6 +66,11 @@ export class FileUploadPageComponent {
   }
 
   get files() {
-    return this.fileUploadForm.get('files');
+    return this.fileUploadForm.get('files') as FormArray;
+  }
+
+  get filesArray() : FormGroup[]
+  {
+    return this.files?.controls as FormGroup[];
   }
 }
